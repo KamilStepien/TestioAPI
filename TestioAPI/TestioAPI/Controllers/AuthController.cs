41e3dbb4c3a13fd0ff4ca812cfd2e8e7290a49d3
KamilStepien <stepienkamil97@gmail.com>
2021-09-03 00:00:08 +0200

    Dodanie kontrolera auth oraz metody logowania

---
+++ TestioAPI/TestioAPI/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TestRestApi.Modles;
+
+namespace TestRestApi.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] LoginModel user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            if (user.UserName == "kamil" && user.Password == "123456789")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superkey1qaz@WSX"));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:44377",
+                    audience: "https://localhost:44377",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signingCredentials
+                    );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+
+            }
+
+            return Unauthorized();
+
+        }
+    }
+}
