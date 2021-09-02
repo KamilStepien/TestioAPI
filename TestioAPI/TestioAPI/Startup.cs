ae9d462bbe842274754a4b73f3d9733c1cf2d545
KamilStepien <stepienkamil97@gmail.com>
2021-09-02 23:55:43 +0200

    Przystosowanie projektu

--- TestioAPI/TestioAPI/Startup.cs
+++ TestioAPI/TestioAPI/Startup.cs
@@ -28,10 +28,7 @@ namespace TestioAPI
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestioAPI", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +37,6 @@ namespace TestioAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestioAPI v1"));
             }
 
             app.UseHttpsRedirection();
