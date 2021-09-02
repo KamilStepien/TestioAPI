6fea1277151e137d030b46d3dcb9970080f4bf2a
KamilStepien <stepienkamil97@gmail.com>
2021-09-02 23:59:00 +0200

    Dodanie kontrolera customers

---
+++ TestioAPI/TestioAPI/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace TestRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<string> Get()
+            => new string[] { "kamil", "dominika" };
+
+    }
+}
