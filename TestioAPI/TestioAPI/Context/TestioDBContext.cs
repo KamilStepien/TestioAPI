98be06a7d184ffd5fe3e62145c59b3fa61ed47e0
KamilStepien <stepienkamil97@gmail.com>
2021-09-08 19:26:28 +0200

    Dodanie klasy bazy w EF

---
+++ TestioAPI/TestioAPI/Context/TestioDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestioAPI.Context
+{
+    public class TestioDBContext:DbContext
+    {
+        public TestioDBContext(DbContextOptions<TestioDBContext> options)
+        : base(options) { }
+      
+
+        public DbSet<Entities.User> Users { get; set; }
+
+
+    }
+}
