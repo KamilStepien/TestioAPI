e824eaff19e0c420c2d48f90cc5093fe884fc82d
KamilStepien <stepienkamil97@gmail.com>
2021-09-08 19:28:43 +0200

    Doodanie modelu  UserRegisterModel

---
+++ TestioAPI/TestioAPI/Modles/Auth/UserRegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestioAPI.Modles.Auth
+{
+    public class UserRegisterModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
