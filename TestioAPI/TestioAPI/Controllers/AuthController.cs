b6018fd47e6bed82188c33da525f8cc71292ecc9
KamilStepien <stepienkamil97@gmail.com>
2021-09-08 19:29:25 +0200

    Medoda rejestracji, hasowanie hasła

--- TestioAPI/TestioAPI/Controllers/AuthController.cs
+++ TestioAPI/TestioAPI/Controllers/AuthController.cs
@@ -3,9 +3,13 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using TestioAPI.Modles;
+using TestioAPI.Context;
+using TestioAPI.Entities;
+using TestioAPI.Modles.Auth;
+using BC = BCrypt.Net.BCrypt;
 
 namespace TestioAPI.Controllers
 {
@@ -13,22 +17,40 @@ namespace TestioAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpPost, Route("login")]
-        public IActionResult Login([FromBody] LoginModel user)
+        private readonly TestioDBContext _context;
+        public AuthController(TestioDBContext context)
         {
-            if (user == null)
+            _context = context;
+        }
+
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] UserLoginModel model)
+        {
+            if (model == null)
             {
-                return BadRequest("Invalid client request");
+                return BadRequest("Model is null");
             }
 
-            if (user.UserName == "kamil" && user.Password == "123456789")
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Model is not valid");
+            }
+
+            var userdb = _context.Users.SingleOrDefault(x => x.Login == model.Login);
+
+            if(userdb == null)
+            {
+                return BadRequest("User don't existing in database ");
+            }
+
+            if (BC.Verify(model.Password, userdb.Password)) 
             {
                 var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superkey1qaz@WSX"));
                 var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
                 var tokenOptions = new JwtSecurityToken(
-                    issuer: "https://localhost:5000",
-                    audience: "https://localhost:5001",
+                    issuer: "https://localhost:44377",
+                    audience: "https://localhost:44377",
                     claims: new List<Claim>(),
                     expires: DateTime.Now.AddMinutes(5),
                     signingCredentials: signingCredentials
@@ -40,7 +62,44 @@ namespace TestioAPI.Controllers
             }
 
             return Unauthorized();
+        }
 
+        [HttpPost, Route("register")]
+        public IActionResult Registger([FromBody] UserRegisterModel model)
+        {
+            if(model == null)
+            {
+                return BadRequest("Model is null");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Model is not valid ");
+            }
+
+            try
+            {
+                var userdb  = _context.Users.FirstOrDefault(x => x.Login == model.Login);
+                if(userdb != null)
+                {
+                    return BadRequest("Login is existing in database");
+                }
+
+                var user = new User
+                {
+                    Login = model.Login,
+                    Password = BC.HashPassword(model.Password),
+                    Email = model.Email
+                };
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok();
         }
     }
 }
