70ac8db2a962f32b99fdda124c9187811238815c
KamilStepien <stepienkamil97@gmail.com>
2021-09-08 23:12:01 +0200

    Enum zawierający stany rezultatu

---
+++ TestioAPI/TestioAPI/Extensions/Results/ResultStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestioAPI.Extensions.Results
+{
+    public enum ResultStatus
+    {
+        Succes,
+        Error
+    }
+}
