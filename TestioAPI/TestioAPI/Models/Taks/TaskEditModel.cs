a2f656106b255306c104f50e56d4d99f929be843
KamilStepien <stepienkamil97@gmail.com>
2021-09-13 16:12:37 +0200

    Dodanie validatorów do klas modeli zadań

--- TestioAPI/TestioAPI/Models/Taks/TaskEditModel.cs
+++ TestioAPI/TestioAPI/Models/Taks/TaskEditModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace TestioAPI.Models.Taks
 {
     public class TaskEditModel
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Description { get; set; }
     }
 }
