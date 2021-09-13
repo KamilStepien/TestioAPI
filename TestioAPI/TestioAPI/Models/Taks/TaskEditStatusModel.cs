034a6cf41d61a51d5e90981aee1539ad41162de4
KamilStepien <stepienkamil97@gmail.com>
2021-09-13 15:30:09 +0200

    Dodanie modelu TaskEditStatusModel

---
+++ TestioAPI/TestioAPI/Models/Taks/TaskEditStatusModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestioAPI.Models.Taks
+{
+    public class TaskEditStatusModel
+    {
+        public int Id { get; set; }
+        public TaskStatusEnum NewStatus { get; set; }
+    }
+}
