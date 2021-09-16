e2ed9b95651512acbe54e73883c03d155a58b92f
KamilStepien <stepienkamil97@gmail.com>
2021-09-16 20:00:40 +0200

    Dodanie atrubutów ograniczeń do modelu UserRegisterModel

--- TestioAPI/TestioAPI/Models/Auth/UserRegisterModel.cs
+++ TestioAPI/TestioAPI/Models/Auth/UserRegisterModel.cs
@@ -5,9 +5,12 @@ namespace TestioAPI.Models.Auth
     public class UserRegisterModel
     {
         [Required]
+        [MaxLength(20)]
         public string Login { get; set; }
         [Required]
+        [MaxLength(20)]
         public string Password { get; set; }
+        [MaxLength(30)]
         public string Email { get; set; }
     }
 }
