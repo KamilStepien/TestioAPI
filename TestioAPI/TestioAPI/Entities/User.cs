f8eb6f192eb741227afffcae08a08580104e5987
KamilStepien <stepienkamil97@gmail.com>
2021-09-16 19:58:01 +0200

    Zmiana typów w tabeli User

--- TestioAPI/TestioAPI/Entities/User.cs
+++ TestioAPI/TestioAPI/Entities/User.cs
@@ -10,12 +10,12 @@ namespace TestioAPI.Entities
         [Key]
         public int Id { get; set; }
         [Required]
-        [Column("u_login", TypeName = "varchar(60)")]
+        [Column("u_login", TypeName = "varchar(20)")]
         public string Login { get; set; }
         [Required]
-        [Column("u_password", TypeName = "varchar(60)")]
+        [Column("u_password", TypeName = "varchar(20)")]
         public string Password { get; set; }
-        [Column("u_email", TypeName = "varchar(60)")]
+        [Column("u_email", TypeName = "varchar(30)")]
         public string Email { get; set; }
 
         public ICollection<Task> Tasks { get; set; }
