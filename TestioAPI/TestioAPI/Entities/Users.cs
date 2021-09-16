d215fe795612a7ccdfc6967f2fe8caf74229b24b
KamilStepien <stepienkamil97@gmail.com>
2021-09-16 20:27:14 +0200

    Zwiekszenie pola password w tabeli users

--- TestioAPI/TestioAPI/Entities/Users.cs
+++ TestioAPI/TestioAPI/Entities/Users.cs
@@ -13,7 +13,7 @@ namespace TestioAPI.Entities
         [Column("u_login", TypeName = "varchar(20)")]
         public string Login { get; set; }
         [Required]
-        [Column("u_password", TypeName = "varchar(20)")]
+        [Column("u_password", TypeName = "varchar(60)")]
         public string Password { get; set; }
         [Column("u_email", TypeName = "varchar(30)")]
         public string Email { get; set; }
